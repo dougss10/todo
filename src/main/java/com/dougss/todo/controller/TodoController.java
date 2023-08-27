9997b54e9d25cc00d312e18634a5259c49425cb5
Douglas <douglas-da.silva.santos@hotmail.com>
2023-08-27 15:42:38 -0300

    first commit

---
+++ src/main/java/com/dougss/todo/controller/TodoController.java
@@ -0,0 +1,16 @@
+package com.dougss.todo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("OK");
+    }
+}
